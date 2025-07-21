5d554103425cadc4b7c07655e63effd36d365aa3
João Eduardo <joaoeduardodev2212@gmail.com>
2025-07-21 17:31:10 -0300

    Add initial script changes and add button classes

---
+++ script.js
@@ -0,0 +1,8 @@
+const display = document.getElementById("calculator-display");
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function(event) {
+        
+    });
+});
