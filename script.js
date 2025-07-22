0067bbfca369c7d9fe2d3d94e93d276a1025192a
João Eduardo <joaoeduardodev2212@gmail.com>
2025-07-22 17:23:14 -0300

    Refactor script loading and clean up commented code in HTML and JavaScript files

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const display = document.getElementById("calculator-display");
 const buttons = document.querySelectorAll(".button");
 
-buttons.forEach(function(button) {
-    button.addEventListener("click", function(event) {
-        
-    });
-});
+// buttons.forEach(function(button) {
+//     button.addEventListener("click", function(event) {
+//         button.classList.contains("number") {
+
+//         }
+//     });
+// });
+
+function hello(name, age) {
+    console.log("Hello " + name + ", your are " + age + " years old.");
+}
+
+hello("Eduardo", 17);
